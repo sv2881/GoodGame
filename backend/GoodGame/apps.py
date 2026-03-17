fbdb89456abc4f5b19d29d8580fde1a987418230
Mitzi Zitler <mitzitler@gmail.com>
2026-03-17 12:34:16 -0400

    For ticket GG-59, the file signals listens for a POST operation on the User api and creates a blank userprofile linked to them, so that attributes can be easily attached (such as profile picture, name, reputation, etc)

--- backend/GoodGame/apps.py
+++ backend/GoodGame/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class GoodgameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'GoodGame'
+
+    def ready(self):
+        import GoodGame.signals  # noqa: F401
